3c1263e9375f7b205196b33d06fce53c3f9cc540
gabsghidini <gabriella.ferreira@xnetweb.com.br>
2024-06-19 22:05:10 -0300

    chore: Initialize project with Create Next App

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,232 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	/* Cost Rate */
+	const [light, setLight] = useState<number>(0);
+	const [water, setWater] = useState<number>(0);
+	const [internet, setInternet] = useState<number>(0);
+	const [rent, setRent] = useState<number>(0);
+	const [hoursPerMonth, setHoursPerMonth] = useState<number>(0);
+	const [profitMargin, setProfitMargin] = useState<number>(0);
+	const [totalCosts, setTotalCosts] = useState<number>(0);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	/* Earning Rate */
+	const [minHourlyRate, setMinHourlyRate] = useState<number>(0);
+	const [hourlyRate, setHourlyRate] = useState<number>(0);
+	const [hoursWorked, setHoursWorked] = useState<number>(0);
+	const [totalEarnings, setTotalEarnings] = useState<number>(0);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const calculateHourlyRate = () => {
+		let totalCosts = light + water + internet + rent;
+		const hourlyRate = parseFloat((totalCosts / hoursPerMonth).toFixed(2));
+		const hourlyRateWithProfit = parseFloat(
+			(hourlyRate + (hourlyRate * profitMargin) / 100).toFixed(2)
+		);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		setTotalCosts(light + water + internet + rent);
+		setMinHourlyRate(hourlyRateWithProfit);
+		setHourlyRate(hourlyRateWithProfit);
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+	const calculateEarnings = () => {
+		const earnings = parseFloat((hourlyRate * hoursWorked).toFixed(2));
+		setTotalEarnings(earnings);
+	};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<div className="min-h-screen flex items-center justify-center bg-gray-100 gap-2">
+			<div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+				<h1 className="text-2xl font-bold text-gray-700 mb-6 text-center">
+					Calculadora de Custo por Hora
+				</h1>
+				<div className="space-y-4">
+					<div>
+						<label
+							htmlFor="light"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Luz
+						</label>
+						<input
+							type="number"
+							id="light"
+							value={light}
+							onChange={(e) =>
+								setLight(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="water"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Água
+						</label>
+						<input
+							type="number"
+							id="water"
+							value={water}
+							onChange={(e) =>
+								setWater(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="internet"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Internet
+						</label>
+						<input
+							type="number"
+							id="internet"
+							value={internet}
+							onChange={(e) =>
+								setInternet(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="rent"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Aluguel
+						</label>
+						<input
+							type="number"
+							id="rent"
+							value={rent}
+							onChange={(e) =>
+								setRent(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="hoursPerMonth"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Quantas horas você trabalha por mês aproximadamente?
+						</label>
+						<input
+							type="number"
+							id="hoursPerMonth"
+							value={hoursPerMonth}
+							onChange={(e) =>
+								setHoursPerMonth(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="profitMargin"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Quantas % você quer de lucro?
+						</label>
+						<input
+							type="number"
+							id="profitMargin"
+							value={profitMargin}
+							onChange={(e) =>
+								setProfitMargin(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<button
+						onClick={() => calculateHourlyRate()}
+						className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700"
+					>
+						Calcular
+					</button>
+				</div>
+				{minHourlyRate !== 0 && (
+					<div className="mt-6 text-center">
+						<p className="text-xl text-gray-700">
+							Você tem um custo mensal de{" "}
+							<span className="font-bold">R$ {totalCosts}</span>{" "}
+							{""} e precisa cobrar no mínimo: {""}
+							<span className="font-bold">
+								R$ {minHourlyRate}
+							</span>
+						</p>
+					</div>
+				)}
+			</div>
+			<div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+				<h1 className="text-2xl font-bold text-gray-700 mb-6 text-center">
+					Calculadora para Projetos
+				</h1>
+				<div className="space-y-4">
+					<div>
+						<label
+							htmlFor="hourlyRate"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Taxa por Hora
+						</label>
+						<input
+							type="number"
+							id="hourlyRate"
+							value={hourlyRate}
+							onChange={(e) =>
+								setHourlyRate(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="hoursWorked"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Horas Trabalhadas
+						</label>
+						<input
+							type="number"
+							id="hoursWorked"
+							value={hoursWorked}
+							onChange={(e) =>
+								setHoursWorked(parseFloat(e.target.value))
+							}
+							className="mt-1 block w-full text-gray-700 p-2 border border-gray-300 rounded-md"
+							required
+						/>
+					</div>
+					<button
+						onClick={() => calculateEarnings()}
+						className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700"
+					>
+						Calcular
+					</button>
+				</div>
+				{totalEarnings !== 0 && (
+					<div className="mt-6 text-center">
+						<p className="text-xl text-gray-700">
+							Custo do Projeto:{" "}
+							<span className="font-bold">R${totalEarnings}</span>
+						</p>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
